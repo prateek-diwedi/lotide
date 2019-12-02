59e3d10e88c8480df4e36e516288d66f7b256839
Prateek Diwedi <diwedi1112@gmail.com>
2019-12-02 22:13:11 +0000

    using linking files of tail and assert equal and making tets cases

--- tail.js
+++ tail.js
@@ -1,12 +1,12 @@
 // FUNCTION IMPLEMENTATION
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 const tail = function (array){
   let final = array.slice(1);
   return final
@@ -14,3 +14,6 @@ const tail = function (array){
 }
 
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(tail([1,2,5,6,3]), [2,5,6,3]);
+
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+
+//assertEqual(words.length, 3); // original array should still have 3 elements!
+//assertEqual(tail([1,2,5,6,3]), [2,5,6,3]);
+
+assertEqual('"Lighthouse Labs"', '"Bootcamp"');
+ assertEqual(1, 1);
+ assertEqual(3, 7);
+
+//module.exports = tail;
