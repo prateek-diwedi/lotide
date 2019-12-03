0545f12ba32bf48af828b1ead4e462ff1d5ff757
Prateek Diwedi <diwedi1112@gmail.com>
2019-12-03 03:05:34 +0000

    testing a code of middle using mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,25 @@
-const middle = require('../middle');
-const eqArrays = require('../eqArrays');
+const eqArrays = require('../eqArrays')
+;
+const assert = require('chai').assert,
+   middle = require('../middle');
 const assertEqual = require('../assertEqual');
+
+
+
+describe('#middle', () => {
+  it("it returns empty space", () => {
+    assert.deepEqual(middle([1, 2],[]),([]));
+  });
+  it("it returns [2, 3] space", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]),([2, 3]));
+  });
+  it("it returns [7, 8] space", () => {
+    assert.deepEqual(middle([5, 7, 8, 12]),([7, 8]));
+  });
+
+});
+
+
 
 assertEqual(eqArrays([1,2,3],[1,2,3]), true);
 assertEqual(eqArrays([1,2,3],[1,9,9,2,1,3]), false);
