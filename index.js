7490f92f062c401bc090e28723341f643a0534c9
Prateek Diwedi <diwedi1112@gmail.com>
2019-12-03 03:28:16 +0000

    export the functions as part of an object.

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetter = require('./countLetter');
+const countOnly = require('./countOnly');
+const assertEqual = require('./assertEqual');
+const eqArray = require('./eqArrays');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetter,
+  countOnly,
+  assertEqual,
+  eqArray
+};
